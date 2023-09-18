cc3e8c451cabf4df1d0e2ba455ccfe51ec87f206
devkhatri523 <devraj.523160@gmail.com>
2023-09-18 15:33:31 +0545

    Password encryption

--- src/main/java/com/example/springmongodb/SpringMongoDbApplication.java
+++ src/main/java/com/example/springmongodb/SpringMongoDbApplication.java
@@ -1,9 +1,11 @@
 package com.example.springmongodb;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringMongoDbApplication {
 
     public static void main(String[] args) {
