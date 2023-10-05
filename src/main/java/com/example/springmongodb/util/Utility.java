06634e46e147d06159835b7f3f6019c6189572a2
devraj <dev.khatri@mercanto.mx>
2023-10-05 19:12:29 +0545

    Change date format

--- src/main/java/com/example/springmongodb/util/Utility.java
+++ src/main/java/com/example/springmongodb/util/Utility.java
@@ -2,7 +2,13 @@ package com.example.springmongodb.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class Utility {
 
@@ -10,5 +16,17 @@ public class Utility {
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEE,dd MMM yyyy HH:mm:ss zzz");
         return dateFormat.parse(date);
 
+    }
+
+    public static void main(String[] args) throws ParseException {
+        String timestamp = "2023-10-04T20:16:30Z";
+        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        inputFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = inputFormat.parse(timestamp);
+        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String georgianDate = outputFormat.format(date);
+        Date date1 = outputFormat.parse(georgianDate);
+
+        System.out.println("Georgian Date: " + date1);
     }
 }
